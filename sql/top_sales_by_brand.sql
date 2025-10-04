0bd852023ffa5549daa4c87ed76819f9c22d7748
giraffe03 <tamgalanbaatar5455@gmail.com>
2025-10-03 21:20:35 -0500

    remove large files from repo

--- sql/top_sales_by_brand.sql
+++ sql/top_sales_by_brand.sql
@@ -1,9 +1,7 @@
 -- Top 10 brands by total sales
 SELECT 
     p.brand,
-    ROUND(SUM(f.price), 2) AS total_sales,
-    COUNT(DISTINCT f.order_id) AS total_orders,
-    COUNT(DISTINCT f.user_id) AS unique_customers
+    ROUND(SUM(f.price), 2) AS total_sales
 FROM fact_sales f
 JOIN dim_product p 
     ON f.product_id = p.product_id
